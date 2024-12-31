4a3e23a0782d71b38feabf6556af5b36472e21ed
Athar Ansari <athar8932@gmail.com>
2024-12-31 14:24:31 +0530

    first commit

--- gifting-tree/sanity.cli.js
+++ gifting-tree/sanity.cli.js
@@ -2,8 +2,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default {
-  projectId: process.env.VITE_SANITY_PROJECT_ID,
-  dataset: process.env.VITE_SANITY_DATASET,
-  useCdn: true,
-  apiVersion: process.env.VITE_SANITY_API_VERSION
+  api: {
+    projectId: process.env.VITE_SANITY_PROJECT_ID,
+    dataset: process.env.VITE_SANITY_DATASET,
+    useCdn: true,
+    apiVersion: process.env.VITE_SANITY_API_VERSION,
+  },
 }
